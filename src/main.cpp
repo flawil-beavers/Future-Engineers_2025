269a8b5a3875e6f90602a1b90423d6c911f5d5d3
Car <robot@car>
2025-04-18 15:53:34 +0100

    Refactor main.cpp: Update gyro initialization, add temperature reading, and improve PID logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <Arduino.h>
 #include <Servo.h>
-#include "PinChangeInterrupt.h"
+#include <PinChangeInterrupt.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_L3GD20_U.h>
+#include <Wire.h>
 
 Servo servo;
 
 // pin definitions
 const int servoPin = 2;
-
 const int enaPin = 11;
 const int in1Pin = 5;
-const int in2Pin = 6;
+const int in2Pin = 8;
 
 const int enTogglePin = 7;
 
@@ -27,7 +29,7 @@ int encoder_dir = 1; // 1 -> CCW, -1 -> CW
 bool en_state = false; // enable state
 
 int max_dc = 200;       // max duty cycle for motor driver
-int min_dc = 25;         // min duty cycle for motor driver
+int min_dc = 25;        // min duty cycle for motor driver
 float max_acc_dc = 255; // max acceleration duty cycle for motor driver (dc/s)
 float current_dc = 0;   // current duty cycle for motor driver
 float acc = 700;        // acceleration speed (mm/s^2)
@@ -36,7 +38,6 @@ bool hold_dc = false;
 
 // speed settings
 float current_speed = 0;
-int _set_speed = 0;
 int target_speed = 0; // target speed for the motor in mm/s
 unsigned long acc_time = 20;
 unsigned long last_acc_time = 0;
@@ -45,28 +46,47 @@ unsigned long last_acc_time = 0;
 int middle = 97; // +55 -55
 int degree_max = middle + 30;
 int degree_min = middle - 30;
+int current_degree = 0;
 int set_degree = 0;
 
 // PID
 float target_distance = 0; // target encoder position in mm
 
-int dc_to_set_temp = 0;
 float measured_speed = 0;   // measured speed in mm/s
 float current_distance = 0; // current distance in mm
 float last_distance = 0;    // last distance in mm
 float Kp = 4.0;             // proportional gain for PID controller
 float Ki = 3.0;             // integral gain for PID controller
 float Kd = 1.0;             // derivative gain for PID controller
-float i_max = 150.0;          // max integral value for PID controller
+float i_max = 150.0;        // max integral value for PID controller
 float pid_integral = 0.0;   // integral term for PID controller
 float last_error = 0.0;     // last error for PID controller
 
 // time variables
 unsigned long current_time = 0;
 unsigned long last_time = 0;
-unsigned long last_status_time = 0; // when the last status was printed
-unsigned long last_loop_time_us = 0;        // last loop time in microseconds
-float last_loop_time = 0;           // last loop time in seconds
+unsigned long last_status_time = 0;  // when the last status was printed
+unsigned long last_loop_time_us = 0; // last loop time in microseconds
+float last_loop_time = 0;            // last loop time in seconds
+
+sensors_event_t event;
+float degree = 0;
+float degree_calibrated = 0;
+float offset = 0;
+float last_offset = 0;
+// float y = -0.0006x - 0.0034
+float offset_m = -0.0006;
+float offset_b = -0.0034;
+float degree_calibrated_temp = 0;
+
+unsigned long last_offset_time = 0;
+const float scaling_calibrated = 1800 / 1750.03; // deg measured for 5 rotations
+
+int temperature = 0;
+float temperature_average = 0; // average temperature since last offset time
+
+// initialise gyro
+Adafruit_L3GD20_Unified gyro;
 
 #define BUFFER_SIZE 64
 
@@ -285,6 +305,92 @@ void steer(int angle)
   servo.write(angle);
 }
 
+int get_temperature()
+{
+  Wire.beginTransmission(L3GD20_ADDRESS);
+  Wire.write(GYRO_REGISTER_OUT_TEMP); // Set read bit
+  Wire.endTransmission(false);        // Send repeated start
+  Wire.requestFrom(L3GD20_ADDRESS, 1);
+
+  if (Wire.available())
+  {
+    temperature = (int8_t)Wire.read(); // Read as signed 8-bit integer
+  }
+  return temperature;
+}
+
+void gyro_config()
+{
+  if (current_time - last_offset_time > 100000000)
+  {
+    last_offset_time = current_time;
+    offset = degree - last_offset;
+    // Serial.print("offset: ");
+    Serial.print(offset / 100, 6);
+    // Serial.print(" temperature: ");
+    Serial.print(", ");
+    Serial.println(temperature_average / 100, 6);
+    temperature_average = 0;
+    last_offset += offset;
+    // delay(100000);
+  }
+}
+
+void pid_config_print()
+{
+  if (current_time - last_status_time > 200000)
+  {
+    last_status_time = current_time;
+    // Serial.print("time passed (ms): ");
+    // Serial.print(last_loop_time * 1000);
+    // Serial.print(" encoder_pos: ");
+    // Serial.print(get_distance(encoder_pos));
+    Serial.print(" target_speed: ");
+    Serial.print(target_speed);
+    Serial.print(" current_speed: ");
+    Serial.print(current_speed);
+    Serial.print(" target_distance: ");
+    Serial.print(target_distance);
+    // Serial.print(" measured_speed: ");
+    // Serial.print(measured_speed);
+    Serial.print(" current_dc: ");
+    Serial.print(current_dc);
+    Serial.print(" kp: ");
+    Serial.print(Kp);
+    Serial.print(" ki: ");
+    Serial.print(Ki);
+    Serial.print(" kd: ");
+    Serial.print(Kd);
+    Serial.print(" dc: ");
+    Serial.print(current_dc);
+    Serial.print(" error: ");
+    Serial.print(target_distance - current_distance);
+    Serial.print(" pid_integral: ");
+    Serial.print(pid_integral);
+    Serial.print(" dc_out: ");
+    Serial.print(dc_out);
+    Serial.print(" pid_before_checking: ");
+    Serial.print(pid_before_checking);
+    Serial.print("\r\n");
+  }
+}
+void gyro_config_print()
+{
+  if (current_time - last_status_time > 200000)
+  {
+    last_status_time = current_time;
+    Serial.print("time passed (ms): ");
+    Serial.print(last_loop_time * 1000);
+    Serial.print(" temperature: ");
+    Serial.print(get_temperature());
+    Serial.print(" degree: ");
+    Serial.print(degree * 180 / PI);
+    Serial.print(" degree_calibrated_temp: ");
+    Serial.print(degree_calibrated_temp * 180 / PI);
+    Serial.print("\r\n");
+  }
+}
+
 void parseMessage(char *msg)
 {
   char cmd[3]; // To store the 2-char command
@@ -306,30 +412,20 @@ void parseMessage(char *msg)
   {
     end++;
   }
-
-  // Serial.println(cmd);
   value = atoi(beg);
-  // Serial.println(value);
-
   switch (cmd[0])
   {
-  // case 'd':
-  //   _set_speed = value;
-  //   break;
+  case 'd':
+    set_speed(value);
+    break;
   case 's':
     set_degree = value;
     break;
-  // case 'e':
-  //   Serial.println(encoder_pos);
-  //   break;
-  case 'c':
-    dc_to_set_temp = value;
+  case 'n':
+    Serial.println(encoder_pos);
     break;
   case 'p':
     emergency_stop();
-    break;
-  case 'd':
-    set_speed(value);
     break;
   case 'q':
     Kp = value / 10.;
@@ -341,18 +437,6 @@ void parseMessage(char *msg)
     Kd = value / 10.;
     break;
   }
-  // if (cmd[0] == 'd')
-  // {
-  //   _set_speed = value;
-  // }
-  // else if (cmd[0] == 's')
-  // {
-  //   set_degree = value;
-  // }
-  // else if (cmd[0] == 'e')
-  // {
-  //   Serial.println(encoder_pos);
-  // }
 }
 
 void processMessage()
@@ -399,6 +483,13 @@ void checkEnable()
   }
 }
 
+void update_gyro()
+{
+  gyro.getEvent(&event);
+  degree += event.gyro.z * last_loop_time;
+  degree_calibrated_temp += (event.gyro.z - (offset_m * get_temperature() + offset_b) / 2) * last_loop_time * scaling_calibrated; // / 2 experimentally included
+}
+
 void update_encoder(int encoderPin)
 {
   int a = digitalRead(encoderPinA);
@@ -443,9 +534,18 @@ void setup()
 
   attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(encoderPinA), update_encoder_a, CHANGE);
   attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(encoderPinB), update_encoder_b, CHANGE);
-}
 
-int pos = 0;
+  delay(30);
+
+  if (!gyro.begin())
+  {
+    /* There was a problem detecting the L3GD20 ... check your connections */
+    Serial.println("Ooops, no L3GD20 detected ... Check your wiring!");
+    while (1)
+      ;
+  }
+  gyro.enableAutoRange(true);
+}
 
 void loop()
 {
@@ -476,54 +576,10 @@ void loop()
     }
   }
   steer(en_state ? set_degree : 0);
-  // drive(en_state ? _set_speed : 0);
   checkEnable();
 
   drive_loop();
-  if (current_dc > 0)
-  {
-    pos ++;
-  }
-  // set_dc(dc_to_set_temp);
-  if (current_time - last_status_time > 200000)
-  {
-    last_status_time = current_time;
-    // Serial.print("time passed (ms): ");
-    // Serial.print(last_loop_time * 1000);
-    // Serial.print(" encoder_pos: ");
-    // Serial.print(get_distance(encoder_pos));
-    Serial.print(" target_speed: ");
-    Serial.print(target_speed);
-    Serial.print(" current_speed: ");
-    Serial.print(current_speed);
-    Serial.print(" target_distance: ");
-    Serial.print(target_distance);
-    // Serial.print(" measured_speed: ");
-    // Serial.print(measured_speed);
-    // Serial.print(" dc_to_set_temp: ");
-    // Serial.print(dc_to_set_temp);
-    Serial.print(" current_dc: ");
-    Serial.print(current_dc);
-    Serial.print(" kp: ");
-    Serial.print(Kp);
-    Serial.print(" ki: ");
-    Serial.print(Ki);
-    Serial.print(" kd: ");
-    Serial.print(Kd);
-    Serial.print(" dc: ");
-    Serial.print(current_dc);
-    Serial.print(" error: ");
-    Serial.print(target_distance - current_distance);
-    Serial.print(" pid_integral: ");
-    Serial.print(pid_integral);
-    Serial.print(" dc_out: ");
-    Serial.print(dc_out);
-    Serial.print(" pid_before_checking: ");
-    Serial.print(pid_before_checking);
-    Serial.print(" pos: ");
-    Serial.print(pos);
-    Serial.print("\r\n");
-  }
+  update_gyro();
 
   last_time = current_time;
   last_distance = current_distance;
